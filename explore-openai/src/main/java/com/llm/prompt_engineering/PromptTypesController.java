10e55f96945956c5712a08df8d5df28d4b84ee0c
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-09-13 21:58:35 +0800

    feat: 很少的镜头提示

--- explore-openai/src/main/java/com/llm/prompt_engineering/PromptTypesController.java
+++ explore-openai/src/main/java/com/llm/prompt_engineering/PromptTypesController.java
@@ -4,6 +4,7 @@ import com.llm.dto.UserInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
@@ -26,6 +27,9 @@ public class PromptTypesController {
     public PromptTypesController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
+
+    @Value("classpath:/prompt-templates/prompt_types/few_shot.st")
+    private Resource fewShotPrompt;
 
     @Value("classpath:/prompt-templates/prompt_types/multi_step_prompt_1.st")
     private Resource multiStep1;
@@ -56,30 +60,33 @@ public class PromptTypesController {
     public String fewShot(@RequestBody UserInput userInput) {
         log.info("userInput : {} ", userInput);
 
-        var fewShotExamples = """
-                Prompt : "The product arrived quickly, worked perfectly, and exceeded my expectations!"
-                Answer : happy
+        String fewShotExamples = """
+                提示：“产品到货很快，运行完美，超出我的预期！”
+                答案：满意
                 
-                Prompt : "Great quality, fast shipping, and exactly as described—highly recommend!"
-                Answer : happy
+                提示：“质量好，发货快，与描述完全一致——强烈推荐！”
+                答案：满意
                 
-                Prompt : "The item arrived broken and didn’t function at all—very disappointing!"
-                Answer : unhappy
+                提示：“商品到货时已损坏，完全无法使用——非常令人失望！”
+                答案：不满意
                 
-                Prompt : "Poor packaging led to a damaged product that was completely useless."
-                Answer : unhappy
+                提示：“包装不当导致产品损坏，完全无法使用。”
+                答案：不满意
                 
                 """;
 
-        var promptMessage = new Prompt(
+        SystemPromptTemplate  systemPromptTemplate = new SystemPromptTemplate(fewShotPrompt);
+        Message systemMessage = systemPromptTemplate.createMessage(Map.of("few_shot_prompts", fewShotExamples));
+        Prompt promptMessage = new Prompt(
                 List.of(
+                        systemMessage,
                         new UserMessage(userInput.prompt())
                 )
         );
 
-        var requestSpec = chatClient.prompt(promptMessage);
+        ChatClient.ChatClientRequestSpec requestSpec = chatClient.prompt(promptMessage);
 
-        var responseSpec = requestSpec.call();
+        ChatClient.CallResponseSpec responseSpec = requestSpec.call();
         return responseSpec.content();
     }
 
